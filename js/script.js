9c9c1aa0036ca11dc2b09181d4a1ed94795834d7
Anton Lut <darthstand@gmail.com>
2012-07-10 10:36:52 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#but_dayx").click(function(){
+        $('#but_dayx').detach();
+        $('#im1').attr("src","img/gif_1.gif").css("display","inline");
+    });
+});
